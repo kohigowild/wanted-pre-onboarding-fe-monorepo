2d77e90f04229c6df670c47af740a05af1ade045
trustmitt <mainmirror14@gmail.com>
2023-05-08 00:17:25 +0900

    feat : 모노레포 내 프로젝트 typecheck 넣기

--- apps/wanted/src/pages/index.tsx
+++ apps/wanted/src/pages/index.tsx
@@ -47,7 +47,7 @@ export default function Home() {
           </div>
         </div>
 
-        <Button>Hello From @wanted/ui</Button>
+        <Button variant="contained">Hello From @wanted/ui</Button>
 
         <div className={styles.center}>
           <Image className={styles.logo} src="/next.svg" alt="Next.js Logo" width={180} height={37} priority />
